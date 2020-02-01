27f40d36965acd7055b90d0d9f96b76f12a9dbe1
samir taha <alrazy1@hotmail.com>
2020-02-01 16:56:14 +0200

    Implemented interfaces and classes for domain.core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/command.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class command : message
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected command() 
+        {
+            Timestamp = DateTime.Now;
+}
+
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/message.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class message : IRequest<bool>
+    {
+        public string MessageType { get; protected set; }
+        protected message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
